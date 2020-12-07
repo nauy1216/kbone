8d5a66c56f0e90af9872f9ad338f1f9bf9106e3d
liuchengyuan <liucy@rp-pet.cn>
2020-12-07 18:36:09 +0800

    update

--- packages/miniprogram-element/tool/webpack.config.js
+++ packages/miniprogram-element/tool/webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
+const dist = path.resolve(__dirname, '../../../examples/demo1/dist/mp/miniprogram_npm/miniprogram-element')
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
         base: path.resolve(__dirname, '../src/base.js'),
     },
     output: {
-        path: path.resolve(__dirname, '../dist'),
+        path: dist,
         filename: '[name].js',
         libraryTarget: 'commonjs2',
     },
@@ -29,16 +30,16 @@ module.exports = {
     plugins: [
         new CopyPlugin({
             patterns: [
-                {from: path.resolve(__dirname, '../src/template'), to: path.resolve(__dirname, '../dist/template')},
-                {from: path.resolve(__dirname, '../src/custom-component'), to: path.resolve(__dirname, '../dist/custom-component')},
-                {from: path.resolve(__dirname, '../src/index.js'), to: path.resolve(__dirname, '../dist/index.js')},
-                {from: path.resolve(__dirname, '../src/index.json'), to: path.resolve(__dirname, '../dist/index.json')},
-                {from: path.resolve(__dirname, '../src/index.wxml'), to: path.resolve(__dirname, '../dist/index.wxml')},
-                {from: path.resolve(__dirname, '../src/index.wxss'), to: path.resolve(__dirname, '../dist/index.wxss')},
-                {from: path.resolve(__dirname, '../src/index-vhost.js'), to: path.resolve(__dirname, '../dist/index-vhost.js')},
-                {from: path.resolve(__dirname, '../src/index-vhost.json'), to: path.resolve(__dirname, '../dist/index-vhost.json')},
-                {from: path.resolve(__dirname, '../src/index-vhost.wxml'), to: path.resolve(__dirname, '../dist/index-vhost.wxml')},
-                {from: path.resolve(__dirname, '../src/index-vhost.wxss'), to: path.resolve(__dirname, '../dist/index-vhost.wxss')},
+                {from: path.resolve(__dirname, '../src/template'), to: path.resolve(dist, 'template')},
+                {from: path.resolve(__dirname, '../src/custom-component'), to: path.resolve(dist, 'custom-component')},
+                {from: path.resolve(__dirname, '../src/index.js'), to: path.resolve(dist, 'index.js')},
+                {from: path.resolve(__dirname, '../src/index.json'), to: path.resolve(dist, 'index.json')},
+                {from: path.resolve(__dirname, '../src/index.wxml'), to: path.resolve(dist, 'index.wxml')},
+                {from: path.resolve(__dirname, '../src/index.wxss'), to: path.resolve(dist, 'index.wxss')},
+                {from: path.resolve(__dirname, '../src/index-vhost.js'), to: path.resolve(dist, 'index-vhost.js')},
+                {from: path.resolve(__dirname, '../src/index-vhost.json'), to: path.resolve(dist, 'index-vhost.json')},
+                {from: path.resolve(__dirname, '../src/index-vhost.wxml'), to: path.resolve(dist, 'index-vhost.wxml')},
+                {from: path.resolve(__dirname, '../src/index-vhost.wxss'), to: path.resolve(dist, 'index-vhost.wxss')},
             ]
         })
     ]
